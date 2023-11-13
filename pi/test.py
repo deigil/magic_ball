521c21f76d1940ae29cb69fb09c6ab38f5970712
deigil <dgilys@hawk.iit.edu>
2023-11-12 18:58:42 -0600

    full integration of code with files for each step

--- pi/test.py
+++ pi/test.py
@@ -1,25 +1,11 @@
-#!/usr/bin/python3
-
-# Capture a full resolution image to memory rather than to a file.
-
+from picamera2 import Picamera2, Preview
 import time
 
-from picamera2 import Picamera2, Preview
-from PIL import Image
-
 picam2 = Picamera2()
-picam2.start_preview(Preview.QTGL)
-preview_config = picam2.create_preview_configuration()
-capture_config = picam2.create_still_configuration()
-
-picam2.configure(preview_config)
+prev_config = picam2.create_preview_configuration(main={"size": (800, 600)})
+picam2.configure(prev_config)
+picam2.start_preview(Preview.QT)
 picam2.start()
-time.sleep(2)
-
-image = picam2.switch_mode_and_capture_image(capture_config)
-mono = Image.open(image).convert("L")
-mono.show()
-
-time.sleep(1)
-
-picam2.close()
+time.sleep(900)
+picam2.stop_preview()
+picam2.stop()
